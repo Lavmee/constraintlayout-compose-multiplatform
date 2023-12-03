8bed84fb8c518e3d90b3d351ff2fac3cdaf3e159
Samuel Gagarin <66745577+Lavmee@users.noreply.github.com>
2023-12-03 15:45:46 +0300

    Remove serialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     alias(libs.plugins.compose).apply(false)
     alias(libs.plugins.android.application).apply(false)
     alias(libs.plugins.android.library).apply(false)
-    alias(libs.plugins.kotlin.serialization).apply(false)
     alias(libs.plugins.maven.publish)
     alias(libs.plugins.spotless)
 }
