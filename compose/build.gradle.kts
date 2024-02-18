f567d1388b2696b21a450c78051b268090e52823
Samuel Gagarin <66745577+Lavmee@users.noreply.github.com>
2024-02-18 23:43:27 +0300

    Update build.gradle.kts

--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -192,7 +192,7 @@ publishing {
             licenses {
                 license {
                     name.set("The Apache Software License, Version 2.0")
-                    url.set("=http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
                 }
             }
             developers {
