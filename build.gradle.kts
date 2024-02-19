f30f3e99507bc35801893b09427bf3a25e76afab
Samuel Gagarin <66745577+Lavmee@users.noreply.github.com>
2024-02-19 21:56:20 +0300

    maven: revert & clear.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.android.application).apply(false)
     alias(libs.plugins.android.library).apply(false)
     alias(libs.plugins.spotless)
-    id("maven-publish")
+    alias(libs.plugins.maven.publish)
 }
 
 allprojects {
--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -1,6 +1,5 @@
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
-import java.util.Properties
 
 // Copyright 2023, Sergei Gagarin and the project contributors
 // SPDX-License-Identifier: Apache-2.0
@@ -9,8 +8,7 @@ plugins {
     alias(libs.plugins.multiplatform)
     alias(libs.plugins.compose)
     alias(libs.plugins.android.library)
-    id("maven-publish")
-    id("signing")
+    alias(libs.plugins.maven.publish)
 }
 
 group = "tech.annexflow.compose"
@@ -122,91 +120,5 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.toVersion(rootProject.extra.get("jvmTarget") as String)
         targetCompatibility = JavaVersion.toVersion(rootProject.extra.get("jvmTarget") as String)
-    }
-}
-
-extra.apply {
-    val publishPropFile = rootProject.file("publish.properties")
-    if (publishPropFile.exists()) {
-        Properties().apply {
-            load(publishPropFile.inputStream())
-        }.forEach { name, value ->
-            if (name == "signing.secretKeyRingFile") {
-                set(name.toString(), rootProject.file(value.toString()).absolutePath)
-            } else {
-                set(name.toString(), value)
-            }
-        }
-    } else {
-        set("signing.keyId", System.getenv("SIGNING_KEY_ID"))
-        set("signing.password", System.getenv("SIGNING_PASSWORD"))
-        set("signing.secretKeyRingFile", System.getenv("SIGNING_SECRET_KEY_RING_FILE"))
-        set("ossrhUsername", System.getenv("OSSRH_USERNAME"))
-        set("ossrhPassword", System.getenv("OSSRH_PASSWORD"))
-    }
-}
-
-val javadocJar by tasks.registering(Jar::class) {
-    archiveClassifier.set("javadoc")
-}
-// https://github.com/gradle/gradle/issues/26091
-val signingTasks = tasks.withType<Sign>()
-tasks.withType<AbstractPublishToMaven>().configureEach {
-    dependsOn(signingTasks)
-}
-
-publishing {
-    if (rootProject.file("publish.properties").exists()) {
-        signing {
-            sign(publishing.publications)
-        }
-        repositories {
-            maven {
-                val releasesRepoUrl =
-                    "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-                val snapshotsRepoUrl =
-                    "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-                url =
-                    if (version.toString().endsWith("SNAPSHOT")) {
-                        uri(snapshotsRepoUrl)
-                    } else {
-                        uri(releasesRepoUrl)
-                    }
-                credentials {
-                    username = project.ext.get("ossrhUsername").toString()
-                    password = project.ext.get("ossrhPassword").toString()
-                }
-            }
-        }
-    }
-
-    publications.withType<MavenPublication> {
-        artifact(javadocJar)
-        pom {
-            artifactId = "constraintlayout-compose-multiplatform"
-            name.set("Compose ConstraintLayout")
-            description.set("Provides Compose ConstraintLayout for building responsive UIs")
-            url.set("https://github.com/lavmee/constraintlayout-compose-multiplatform/")
-            inceptionYear.set("2023")
-
-            licenses {
-                license {
-                    name.set("The Apache Software License, Version 2.0")
-                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                }
-            }
-            developers {
-                developer {
-                    id.set("lavmee")
-                    name.set("Sergei Gagarin")
-                    email.set("lavmeejetski@gmail.com")
-                }
-            }
-            scm {
-                url.set("scm:git:git://github.com/lavmee/constraintlayout-compose-multiplatform.git")
-                connection.set("scm:git:git://github.com/lavmee/constraintlayout-compose-multiplatform.git")
-                developerConnection.set("scm:git:git://github.com/lavmee/constraintlayout-compose-multiplatform.git")
-            }
-        }
     }
 }
