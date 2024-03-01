4472a5d133b2758173c7def3502e5a060481987c
Samuel Gagarin <66745577+Lavmee@users.noreply.github.com>
2024-03-01 19:05:36 +0300

    lib: bump version to 0.3.0.
    deps: update compose -> 1.6.0.
    sample: remove deprecated icons.

--- sample/shared/src/commonMain/kotlin/tech/annexflow/sample/AllDemos.kt
+++ sample/shared/src/commonMain/kotlin/tech/annexflow/sample/AllDemos.kt
@@ -28,7 +28,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -98,7 +98,7 @@ internal fun ComposeConstraintLayoutDemos() {
                             verticalAlignment = Alignment.CenterVertically,
                         ) {
                             Icon(
-                                imageVector = Icons.Filled.ArrowBack,
+                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                                 contentDescription = "Back",
                             )
                             Text(text = composeDemo.title)
