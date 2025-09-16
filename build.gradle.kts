348f5e1e5c999d3bb0045bd9b72053dca78afb8c
Samuel Gagarin <66745577+Lavmee@users.noreply.github.com>
2025-09-16 17:38:09 +0300

    build(gradle): bump all deps & migrate -> kmm android kotlin plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,35 +2,8 @@ plugins {
     alias(libs.plugins.multiplatform).apply(false)
     alias(libs.plugins.compose).apply(false)
     alias(libs.plugins.android.application).apply(false)
-    alias(libs.plugins.android.library).apply(false)
+    alias(libs.plugins.android.kotlin.library).apply(false)
     alias(libs.plugins.maven.publish)
-    alias(libs.plugins.spotless)
-}
-
-allprojects {
-    apply(plugin = "com.diffplug.spotless")
-
-    spotless {
-        kotlin {
-            target("**/*.kt")
-            targetExclude("${layout.buildDirectory}/**/*.kt")
-            targetExclude("bin/**/*.kt")
-            ktlint().editorConfigOverride(
-                mapOf(
-                    "standard:no-consecutive-comments" to "disabled",
-                    "ktlint_standard_property-naming" to "disabled",
-                    "ktlint_standard_function-naming" to "disabled",
-                    "ktlint_standard_no-empty-file" to "disabled",
-                ),
-            )
-        }
-
-        kotlinGradle {
-            target("**/*.kts")
-            targetExclude("${layout.buildDirectory}/**/*.kts")
-            ktlint()
-        }
-    }
 }
 
 extra.apply {
